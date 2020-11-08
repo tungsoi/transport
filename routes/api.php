8c4d7d97b1a1708e198a0917d41579cbb8115781
tungsoi <thanhtung.atptit@gmail.com>
2020-11-09 01:04:21 +0700

    fixbug

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use App\Admin\Services\PurchaseOrderService;
 use App\Models\Aloorder\OrderItem;
+use App\Models\Aloorder\PurchaseOrder;
+use App\Models\TransportRecharge;
+use App\User;
+use Encore\Admin\Facades\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -32,6 +36,31 @@ Route::post('/confirm-receive-vietnam', function (Request $request) {
         $item = OrderItem::find($item_id);
         $item->status = OrderItem::STATUS_PURCHASE_WAREHOUSE_VN;
         $item->save();
+
+        $order = PurchaseOrder::find($item->order_id);
+        if ($order->warehouseVietnamItems() == $order->totalItems()) {
+            $order->status = PurchaseOrder::STATUS_SUCCESS;
+            $order->save();
+
+            $deposited = $order->deposited;
+            $total_final_price = $order->final_total_price;
+
+            $owed = $total_final_price - $deposited;
+
+            $customer = User::find($order->customer_id);
+            $wallet = $customer->wallet;
+            $customer->wallet = $wallet - $owed;
+            $customer->save();
+
+            TransportRecharge::create([
+                'customer_id'       =>  $order->customer_id,
+                'user_id_created'   =>  Admin::user()->id,
+                'money' =>  $owed > 0 ? $owed : -($owed),
+                'type_recharge' =>  TransportRecharge::PAYMENT_ORDER,
+                'content'   =>  'Thanh toán đơn hàng mua hộ. Mã đơn hàng '.$order->order_number.". Số tiền " . number_format($owed),
+                'order_type'    =>  TransportRecharge::TYPE_ORDER
+            ]);
+        }
 
         DB::commit();
     

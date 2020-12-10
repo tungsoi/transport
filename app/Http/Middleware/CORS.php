e37facf7c9ce3dfa41370e90f0e20201e178222a
tungsoi <thanhtung.atptit@gmail.com>
2020-12-10 11:37:43 +0700

    fixbug

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -14,12 +14,15 @@ class CORS
      * @return mixed
      */
     public function handle($request, Closure $next)
-     {
-     	return $next($request)
-      		->header('Access-Control-Allow-Origin', '*')
-      		->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-      		->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
-      }
+    {
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin' , '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+        $response->header->set('Access-Control-Allow-Credentials', 'true');
+ 
+        return $response;
+    }
 
 
 }

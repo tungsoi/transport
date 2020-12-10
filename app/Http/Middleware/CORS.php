e78ae4c22e1f0973cb7fd2aff088bf689e4c6dbe
tungsoi <thanhtung.atptit@gmail.com>
2020-12-10 10:27:59 +0700

    fixbug

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -16,7 +16,7 @@ class CORS
     public function handle($request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', '*');
+        ->header('Access-Control-Allow-Origin: *')
+        ->header('Access-Control-Allow-Methods: *');
     }
 }

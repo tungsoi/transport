dcd0d70a5ceb5bc8422b4b7d596422554616dc34
tungsoi <thanhtung.atptit@gmail.com>
2021-01-16 17:19:20 +0700

    fixbug

--- app/Admin/Controllers/ReportWarehouse/TransportRouteController.php
+++ app/Admin/Controllers/ReportWarehouse/TransportRouteController.php
@@ -2,11 +2,13 @@
 
 namespace App\Admin\Controllers\ReportWarehouse;
 
+use App\Models\ReportWarehouse;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 use App\Models\TransportRoute;
+use Encore\Admin\Facades\Admin;
 
 class TransportRouteController extends AdminController
 {
@@ -67,20 +69,200 @@ class TransportRouteController extends AdminController
      */
     protected function detail($id)
     {
-        $show = new Show(TransportRoute::findOrFail($id));
+        $grid = new Grid(new ReportWarehouse());
 
-        $show->field('id', trans('admin.id'));
-        $show->title(trans('admin.title'));
-        $show->field('created_at', trans('admin.created_at'));
-        $show->field('updated_at', trans('admin.updated_at'));
-        $show->panel()
-        ->tools(function ($tools) {
-            $tools->disableEdit();
-            $tools->disableList();
-            $tools->disableDelete();
-        });;
-        
-        return $show;
+        $grid->model()->select('date')->groupBy('date')->orderBy('date', 'desc');
+
+        $grid->filter(function($filter) {
+            $filter->expand();
+            $filter->disableIdFilter();
+            $filter->between('date', "Ngày")->date();
+        });
+
+        $grid->header(function ($query) use ($id) {
+            return "<h3>Line " . TransportRoute::find($id)->title."</h3>";
+        });
+        $routes = TransportRoute::where('id', $id)->get();
+
+        $grid->column('date',"Ngày")->totalRow(function () {
+            return "-";
+        });
+        $grid->column('total', 'Số lượng')->display(function () use ($routes) {
+            
+            $html = "";
+            $total = 0;
+            foreach ($routes as $route) {
+                $content = ReportWarehouse::where('date', $this->date)
+                ->whereIn('line', [0, 1])
+                ->whereTransportRoute($route->id)
+                ->count();
+                $total += $content;
+
+                $html .= "<span class='sum_total'>".$content."</span>";
+            }
+
+            return $html;
+        });
+        $grid->column('weight', 'Tổng KG')->display(function () use ($routes) {
+            $html = "";
+            $total = 0;
+            foreach ($routes as $route) {
+                $content = ReportWarehouse::where('date', $this->date)
+                ->whereTransportRoute($route->id)
+                ->sum('weight');
+                $total += $content;
+
+                $html .= "<span class='sum_weight'>".$content."</span>";
+            }
+            
+            return $html;
+        });
+        $grid->column('cublic_meter', 'Tổng M3')->display(function () use ($routes) {
+            $html = "";
+            $total = 0;
+            foreach ($routes as $route) {
+                $content = ReportWarehouse::where('date', $this->date)
+                ->whereTransportRoute($route->id)
+                ->sum('cublic_meter');
+                $total += $content;
+
+                $html .= "<span class='sum_cublic_meter'>".$content."</span>";
+            }
+
+            return $html;
+        });
+        $grid->column('box', 'Số bao')->display(function () use ($routes) {
+            $html = "";
+            $total = 0;
+            foreach ($routes as $route) {
+                $content = ReportWarehouse::where('date', $this->date)
+                ->whereTransportRoute($route->id)
+                ->where('line', 0)->count();
+                $total += $content;
+
+                $html .= "<span class='sum_box'>".$content."</span>";
+            }
+
+            return $html;
+        });
+        $grid->column('kg_box', 'Số KG bao')->display(function () use ($routes) {
+            $html = "";
+            $total = 0;
+            foreach ($routes as $route) {
+                $content = ReportWarehouse::where('date', $this->date)
+                ->whereTransportRoute($route->id)
+                ->where('line', 0)
+                ->sum('weight');
+                $total += $content;
+
+                $html .= "<span class='sum_kg_box'>".$content."</span>";
+            }
+
+            return $html;
+        });
+        $grid->column('m3_box', 'M3 bao')->display(function () use ($routes) {
+            $html = "";
+            $total = 0;
+            foreach ($routes as $route) {
+                $content = ReportWarehouse::where('date', $this->date)
+                ->whereTransportRoute($route->id)
+                ->where('line', 0)
+                ->sum('cublic_meter');
+                $total += $content;
+
+                $html .= "<span class='sum_m3_box'>".$content."</span>";
+            }
+
+            return $html;
+        });
+        $grid->column('package', 'Số kiện')->display(function () use ($routes) {
+            $html = "";
+            $total = 0;
+            foreach ($routes as $route) {
+                $content = ReportWarehouse::where('date', $this->date)
+                ->whereTransportRoute($route->id)
+                ->where('line', 1)
+                ->count();
+                $total += $content;
+
+                $html .= "<span class='sum_package'>".$content."</span>";
+            }
+
+            return $html;
+        });
+        $grid->column('kg_package', 'Số KG kiện')->display(function () use ($routes) {
+            $html = "";
+            $total = 0;
+            foreach ($routes as $route) {
+                $content = ReportWarehouse::where('date', $this->date)
+                ->whereTransportRoute($route->id)
+                ->where('line', 1)
+                ->sum('weight');
+                $total += $content;
+
+                $html .= "<span class='sum_kg_package'>".$content."</span>";
+            }
+
+            return $html;
+        });
+        $grid->column('m3_package', 'M3 kiện')->display(function () use ($routes) {
+            $html = "";
+            $total = 0;
+            foreach ($routes as $route) {
+                $content = ReportWarehouse::where('date', $this->date)
+                ->whereTransportRoute($route->id)
+                ->where('line', 1)
+                ->sum('cublic_meter');
+                $total += $content;
+
+                $html .= "<span class='sum_m3_package'>".$content."</span>";
+            }
+
+            return $html;
+        });
+
+        // setup
+        $grid->paginate(20);
+        $grid->disableActions();
+
+            Admin::script(
+                <<<EOT
+    
+                $('tfoot').each(function () {
+                    $(this).insertAfter($(this).siblings('thead'));
+                });
+    
+                $( document ).ready(function() {
+                    $('tfoot .column-date').html("");
+    
+                    $('tfoot .column-total').html(getTotal('sum_total', 0));
+                    $('tfoot .column-weight').html(getTotal('sum_weight', 1));
+                    $('tfoot .column-cublic_meter').html(getTotal('sum_cublic_meter'));
+                    $('tfoot .column-box').html(getTotal('sum_box', 0));
+                    $('tfoot .column-kg_box').html(getTotal('sum_kg_box', 2));
+                    $('tfoot .column-m3_box').html(getTotal('sum_m3_box'));
+                    $('tfoot .column-package').html(getTotal('sum_package', 0));
+                    $('tfoot .column-kg_package').html(getTotal('sum_kg_package', 2));
+                    $('tfoot .column-m3_package').html(getTotal('sum_m3_package'));
+                });
+    
+                function getTotal(cl, prefix = 4) {
+                    let obj = $('tbody .'+cl);
+    
+                    let total = 0;
+                    obj.each( function( i, el ) {
+                        var elem = $( el );
+                        let html = parseFloat($.trim(elem.html()));
+                        total += html;
+                    });
+                    total = total.toFixed(prefix);
+    
+                    return total;
+                }
+EOT
+    );
+
+        return $grid;
     }
 
     /**
